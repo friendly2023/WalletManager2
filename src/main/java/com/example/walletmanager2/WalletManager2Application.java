e3d9f3804c893cc3f545332639156db38a8153af
Vladimir <kazimirov.2013@mail.ru>
2025-05-11 16:22:56 +0300

    initialized  project

---
+++ src/main/java/com/example/walletmanager2/WalletManager2Application.java
@@ -0,0 +1,13 @@
+package com.example.walletmanager2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletManager2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletManager2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/walletmanager2/WalletManager2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletmanager2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletManager2ApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
